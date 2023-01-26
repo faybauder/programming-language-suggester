851b1b025b16c6aeea5c9b368fdccd05676cb3e2
Fay Bauder <fay.bauder@gmail.com>
2023-01-26 14:25:14 -0800

    add background color and line up answer inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Business Logic
-
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+  }
+}
 
 // User Interface Logic
