d0fb07aefc38615a04319e9c6c6dcba860b3c28a
Fay Bauder <fay.bauder@gmail.com>
2023-01-26 19:25:09 -0800

    add some js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 // Business Logic
-window.onload = function() {
-  const form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-  }
-}
+
 
 // User Interface Logic
+
+
+  form.addEventListener("submit", function() {
+    const answer1 = document.getElementById("question1");
+    const answer2 = document.getElementById("question2");
+    const answer3 = document.getElementById("question3");
+    const answer4 = document.getElementById("question4");
+    const answer5 = document.getElementById("question5");
+
+    document.querySelector("") = 
+    document.querySelector("") = 
+    document.querySelector("") = 
+    document.querySelector("") = 
+    document.querySelector("") = 
+  });
+}
